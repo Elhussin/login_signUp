e0aae3d8268650eeb015a5a1f0c5a6f22615b756
Elhussini Taha <hasin3112@gmail.com>
2022-12-27 13:24:37 -0800

    update hader index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html >
+<html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="shortcut icon" href="/img/logo.jpg">
-    
-    <title>Hussain</title>
 
+    <title>Hussain</title>
+    <title>Project</title>
     <!-- <style>
         img{
          
@@ -16,13 +17,14 @@
     </style> -->
 
 </head>
+
 <body>
 
-<?php require_once "nav.php";?>
-    
+    <?php require_once "nav.php";?>
+
 
 
 
 </body>
-</html>
 
+</html>
